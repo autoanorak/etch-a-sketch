da8bc88d04d7d1d95e3d14ee5cdcf899006432ae
David Yu <32144987+autoanorak@users.noreply.github.com>
2023-07-12 11:34:43 -0700

    Update grid creation methodology and refactor code

--- scripts.js
+++ scripts.js
@@ -1,42 +1,49 @@
-const container = document.querySelector('.container');
+const grid = document.querySelector('.grid');
+const reSizeBtn = document.querySelector('#reSizeBtn');
+const eraserModeBtn = document.querySelector('#eraserModeBtn');
 
-const button = document.querySelector('#buttonForSize');
-button.addEventListener('click', promptUserForSize);
-let size;
+reSizeBtn.addEventListener('click', promptUserForSize);
+eraserModeBtn.addEventListener('click', eraserMode);
 
-createDivs();  
+let size = 16;
+let erase = false;
+
+updateGrid();  
 addListeners();
 
 function promptUserForSize() {
   do {
     size = parseInt(prompt('Size of grid:'));
   } while (size > 100 || size < 1);
-  
+
   removeDivs();
-  createDivs();  
-  addListeners();
+  updateGrid();  
 }
 
 
-function createDivs() {
-  let rowDiv;
-  let itemDiv;
+function updateGrid() { 
+let newDiv;
 
-  if (size === undefined) {
-    size = 16;
-  }
-  
+grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
   for (let row = 0; row < size; row++) {
-    rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
-    container.appendChild(rowDiv);
-
     for (let item = 0; item < size; item++) {
-      itemDiv = document.createElement('div');
-      itemDiv.classList.add('item')
-      rowDiv.appendChild(itemDiv);  
+      newDiv = document.createElement('div');
+      newDiv.classList.add('item')
+      grid.appendChild(newDiv);
+      newDiv.addEventListener('mousedown', sketch); // sketches on single click
     }
   }
+
+grid.addEventListener('mousedown', () => {
+  grid.addEventListener('mouseover', sketch); // sketches on click + drag
+});
+
+window.addEventListener('mouseup', () => {
+  grid.removeEventListener('mouseover', sketch); // stops sketching when mouse released
+})
+
 }
 
 
@@ -54,21 +61,18 @@ function removeDivs() {
 }
 
 
-function addListeners() {
-  const divs = document.querySelectorAll('div.item');
-
-  divs.forEach((item) => {
-    item.addEventListener('mouseover', hoverEffectOn);
-    // item.addEventListener('mouseout', hoverEffectOff);
-  })  
+function eraserMode() {
+  erase = true;
 }
 
 
-function hoverEffectOn(e) {
-  e.target.classList.add('hover');
-}
+function sketch(e) {
+  console.log(e.target);
+  console.log(e.currentTarget);
 
-
-function hoverEffectOff(e) {
-  e.target.classList.remove('hover');
+  if (erase === false) {
+    e.target.classList.add('etched');
+  } else {
+    e.target.classList.remove('etched');
+  }
 }
