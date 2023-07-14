2097d1c0b9492e9790c2102900c109bada9546ed
David Yu <32144987+autoanorak@users.noreply.github.com>
2023-07-13 23:14:28 -0700

    Add 'random mode', update styling

--- scripts.js
+++ scripts.js
@@ -1,27 +1,41 @@
 /* Declare consts for HTML elements */
+
 const grid = document.querySelector('.grid');
 const reSizeBtn = document.querySelector('#reSizeBtn');
+const clearBtn = document.querySelector('#clearBtn');
 const eraserBtn = document.querySelector('#eraserBtn');
-const progressiveBtn = document.querySelector('#progressiveBtn')
+const progressiveBtn = document.querySelector('#progressiveBtn');
+const randomBtn = document.querySelector('#randomBtn');
+
 
 /* Add event listeners to buttons */
+
 reSizeBtn.addEventListener('click', promptUserForSize);
+clearBtn.addEventListener('click', updateGrid);
 eraserBtn.addEventListener('click', eraserMode);
 progressiveBtn.addEventListener('click', progressiveMode);
+randomBtn.addEventListener('click', randomMode);
+
 
 /* Declare and initialize variables to be used in functions */
+
 let size = 16;
 let erase = false;
 let progressive = false;
+let random = false;
 let rgbStart = [];
 let rgbCurrent = [];
 let rgbTarget = [];
 let rgbTravel = [];
 
+
 /* Render basic 16x16 grid */
+
 updateGrid();  
 
+
 /* Functions for basic features */
+
 function promptUserForSize() {
   do {
     size = parseInt(prompt('Size of grid:'));
@@ -32,8 +46,7 @@ function promptUserForSize() {
 
 
 function updateGrid() { 
-  // First, remove existing grid
-  removeGrid();
+  clearGrid();
 
   let newDiv;
 
@@ -63,10 +76,38 @@ function updateGrid() {
 }
 
 
-function removeGrid() {
+function clearGrid() {
   let items = document.querySelectorAll('.item');
-
   items.forEach(item => { item.remove(); })
+}
+
+
+function sketch(e) {
+  switch (erase) {
+    case true:
+      eraseSketch(e);
+      return;
+  }
+  
+  switch (progressive) {
+    case true:
+      progressiveSketch(e);
+      return;
+  }
+
+  switch (random) {
+    case true:
+      randomSketch(e);
+      return;
+  }
+
+  if (e.target.classList.contains('progressive-etched')) {
+    e.target.classList.remove('progressive-etched');
+  } else if (e.target.classList.contains('random-etched')) {
+    e.target.classList.remove('random-etched');
+  }
+  
+  e.target.style.backgroundColor = 'rgb(0, 0, 0)';
 }
 
 
@@ -81,6 +122,13 @@ function eraserMode(e) {
 }
 
 
+function eraseSketch(e) {
+  e.target.style.backgroundColor = 'inherit';
+}
+
+
+/* Functions for progressive darkening feature */
+
 function progressiveMode() {
   progressiveBtn.classList.toggle('clicked');
 
@@ -89,45 +137,35 @@ function progressiveMode() {
   } else {
     progressive = false;
   }
+
+  // Prevent simultaneous activation of random mode and progressive mode
+  randomBtn.classList.remove('clicked');
+  random = false;
 }
 
 
-function sketch(e) {
-  if (erase === false) {
-    if (progressive === false) {
-      e.target.classList.add('etched');
-    } else {
-      // Store current RGB value as array of integers
-      rgbCurrent = rgbToArray(getComputedStyle(e.target).getPropertyValue('background-color').slice(4, -1));
+function progressiveSketch(e) {  
+  // Store current RGB value as array of integers
+  rgbCurrent = rgbToArray(getComputedStyle(e.target).getPropertyValue('background-color').slice(4, -1));
+  
+  // If virgin square, calculate "travel" from current RGB value to target RGB value
+  if (!e.target.classList.contains('progressive-etched')) {
+    rgbStart = rgbCurrent;
+    rgbTarget = rgbToArray('0, 0, 0');
+    rgbTravel = computeTravel(rgbStart, rgbTarget);
+    e.target.classList.add('progressive-etched');
+  } 
 
-      /* 
-       * If square has not yet been progressive etched, calculate the "travel"
-       * from current RGB value to target RGB value
-       */ 
-      if (!e.target.classList.contains('progressive-etched')) {
-        rgbStart = rgbCurrent;
-        rgbTarget = rgbToArray('0, 0, 0');
-        rgbTravel = computeTravel(rgbStart, rgbTarget);
-        e.target.classList.add('progressive-etched');
-      } 
-
-      // Calculate what the next "stage" in the progressive RGB values should be
-      for (let i = 0; i < 3; i++) {
-        rgbCurrent[i] = Math.max(rgbCurrent[i] + (rgbTravel[i] / 10), 0);
-      }
-
-      // Change the element's RGB value
-      e.target.style.backgroundColor = `rgb(${rgbCurrent[0]}, ${rgbCurrent[1]}, ${rgbCurrent[2]})`;
-    }
-    
-  } else {
-    // If "erase" mode engaged, remove the etch
-    e.target.classList.remove('etched');
+  // Calculate what the next "stage" in the progressive RGB values should be
+  for (let i = 0; i < 3; i++) {
+    rgbCurrent[i] = Math.max(rgbCurrent[i] + (rgbTravel[i] / 10), 0);
   }
+
+  // Change the element's RGB value
+  e.target.style.backgroundColor = `rgb(${rgbCurrent[0]}, ${rgbCurrent[1]}, ${rgbCurrent[2]})`;
 }
 
 
-/* Functions for progressive darkening feature */
 function rgbToArray(rgb) {
   // Parse background-color RGB string value into array of integers
   let rgbArray = rgb.split(',');
@@ -143,10 +181,40 @@ function rgbToArray(rgb) {
 
 function computeTravel(rgbCurrent, rgbTarget) {
   let travel = [];
-  
+
   for (let i = 0; i < 3; i++) {
     travel[i] = rgbTarget[i] - rgbCurrent[i];
   }
 
   return travel;
+}
+
+
+/* Functions for random RGB drawing feature */
+
+function randomMode() {
+  randomBtn.classList.toggle('clicked');
+
+  if (random === false) {
+    random = true;
+  } else {
+    random = false;
+  }
+
+  // Prevent simultaneous activation of progressive mode and random mode
+  progressiveBtn.classList.remove('clicked');
+  progressive = false;
+}
+
+
+function randomSketch(e) {
+  if (!e.target.classList.contains('random-etched')) {
+    e.target.classList.add('random-etched');
+  } 
+
+  let randomR = Math.floor(Math.random() * 256);
+  let randomG = Math.floor(Math.random() * 256);
+  let randomB = Math.floor(Math.random() * 256);
+
+  e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;  
 }
